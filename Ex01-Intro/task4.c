faf4cc6a1b8e1b3e4f57cbf77d2a2ed78e467dfa
Glenn Bitar <glennbitar@gmail.com>
2016-02-15 05:06:52 +0100

    dunno

--- Ex01-Intro/task4.c
+++ Ex01-Intro/task4.c
@@ -1,33 +1,50 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-int64_t i = 0;
+#define ITER 10000
+
+int32_t i = 0;
+sem_t mutex;
 
 void* thread_1()
 {
-	for (int j = 0; j < 1000000; j++)
+	for (int j = 0; j < ITER; j++)
 	{
+		sem_wait(&mutex);
+		//printf("Thread 1\n");
 		i++;
+		sem_post(&mutex);
 	}
+	printf("t1 completed\n");
 	return NULL;
 }
 
 void* thread_2()
 {
-	for (int j = 0; j < 1000000; j++)
+	for (int j = 0; j < ITER; j++)
 	{
+		sem_wait(&mutex);
+		//printf("Thread 2\n");
 		i--;
+		sem_post(&mutex);
 	}
+	printf("t2 completed\n");
 	return NULL;
 }
 
 int main()
 {
+	sem_init(&mutex, 0, 1);
+
 	pthread_t t1;
 	pthread_t t2;
+
+	printf("Starting t1\n");
 	pthread_create(&t1, NULL, thread_1, NULL);
-	pthread_create(&t2, NULL, thread_2, NULL);
+	printf("Starting t2\n");
+	pthread_create(&t2, NULL, thread_2, NULL);	
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 	printf("%i\n", (int32_t)i);
